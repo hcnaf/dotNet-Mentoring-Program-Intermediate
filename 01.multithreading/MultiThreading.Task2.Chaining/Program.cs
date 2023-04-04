90a9d0ed65d834d2ffce80e89b0f50eb1c8cebba
Anh Tuan Phan Tran <Anh_Tuan_Phan_Tran@epam.com>
2023-04-04 20:17:57 +0300

    Implement Task2.Chaining

--- 01.multithreading/MultiThreading.Task2.Chaining/Program.cs
+++ 01.multithreading/MultiThreading.Task2.Chaining/Program.cs
@@ -6,10 +6,12 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
@@ -21,9 +23,61 @@ namespace MultiThreading.Task2.Chaining
             Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
             Console.WriteLine();
 
-            // feel free to add your code
+            Task<int[]> task1 = new Task<int[]>(() =>
+            {
+                var random = new Random();
+                var values = new int[10];
+
+                for (int i = 0; i < values.Length; ++i)
+                    values[i] = random.Next(1, 100);
+
+                Console.WriteLine(values.ToView());
+
+                Console.WriteLine();
+
+                return values;
+            });
+
+
+            Task<int[]> task2 = task1.ContinueWith(previous =>
+            {
+                Random random = new Random();
+                int randomValue = random.Next(1, 100);
+
+                Console.WriteLine($"Random value: {randomValue}");
+
+                for (int i = 0; i < previous.Result.Length; ++i)
+                    previous.Result[i] *= randomValue;
+
+                Console.WriteLine(previous.Result.ToView());
+
+                Console.WriteLine();
+
+                return previous.Result;
+            });
+
+            Task<int[]> task3 = task2.ContinueWith(previous =>
+            {
+                Array.Sort(previous.Result);
+
+                Console.WriteLine(previous.Result.ToView());
+
+                Console.WriteLine($"Current Task: {Task.CurrentId}  Previous Task: {previous.Id}");
+
+                return previous.Result;
+            });
+
+
+            Task task4 = task3.ContinueWith(previous =>
+                Console.WriteLine($"Average value: {previous.Result.Average()}"));
+
+            task1.Start();
+            task4.Wait();
 
             Console.ReadLine();
         }
+
+        public static string ToView(this int[] array)
+            => string.Join(", ", array);
     }
 }
