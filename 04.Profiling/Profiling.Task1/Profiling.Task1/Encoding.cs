130a8e1c53d621365a8baab37a2267ef5610370a
Anh Tuan Phan Tran <Anh_Tuan_Phan_Tran@epam.com>
2023-05-11 21:48:29 +0300

    Implement 04.Profiling Task1, Load Task2, Task3

---
+++ 04.Profiling/Profiling.Task1/Profiling.Task1/Encoding.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+
+namespace Profiling.Task1;
+
+
+internal static class Encoding
+{
+    public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+    {
+
+        var iterate = 10000;
+        var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+        byte[] hash = pbkdf2.GetBytes(20);
+
+        byte[] hashBytes = new byte[36];
+        Array.Copy(salt, 0, hashBytes, 0, 16);
+        Array.Copy(hash, 0, hashBytes, 16, 20);
+
+        var passwordHash = Convert.ToBase64String(hashBytes);
+
+        return passwordHash;
+
+    }
+    public static string GeneratePasswordHashUsingSalt_Span(string passwordText, byte[] salt)
+    {
+        var iterate = 10000;
+        var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate, HashAlgorithmName.SHA256);
+        byte[] hash = pbkdf2.GetBytes(20);
+
+        Span<byte> hashBytes = stackalloc byte[36];
+        salt.CopyTo(hashBytes[0..16]);
+        hash.CopyTo(hashBytes[16..36]);
+
+        var passwordHash = Convert.ToBase64String(hashBytes);
+
+        return passwordHash;
+    }
+}
---
+++ 04.Profiling/Profiling.Task1/Profiling.Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Profiling.Task1;
+
+byte[] salt = new byte[] { 56, 78, 66, 45, 128, 40, 67, 23, 54, 0, 0, 0, 0, 0, 0, 1 };
+string[] passwords = new string[]
+{
+    "1234567!53421",
+    "QwertyUiopasdfghJKl;'zxcb,klkjsdhfdgajdha;sdkjkjsdlfja;zskdljza;sdh;",
+    "123",
+    "*&^(*&^%&^#$&(*&%^$%&^%$)",
+    "Password12",
+    "12345678901234567890-1237567777q777777q777777777777777q7777777777777777q777777777777777777q777777777777q777777777777777q777777777777q777777",
+    "123123123123123123123123123123123123123123123123123123123123123123123123123123123",
+};
+
+for (int i = 0; i < passwords.Length; i += 1)
+{
+    Console.WriteLine($"For password - '{passwords[i]}'");
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    var hash = Encoding.GeneratePasswordHashUsingSalt(passwords[i], salt);
+    stopwatch.Stop();
+    Console.WriteLine($"\tSource algorithms: {stopwatch.ElapsedTicks} ticks");
+    stopwatch.Restart();
+    var hash2 = Encoding.GeneratePasswordHashUsingSalt_Span(passwords[i], salt);
+    stopwatch.Stop();
+    Console.WriteLine($"\tChanged algorithms: {stopwatch.ElapsedTicks} ticks");
+}
+
